37839d0636be1aa9f87a4531e4048d9840142fd1
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-07 13:19:02 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,7 +2,7 @@ HEROKU = True # Make it False if you're not deploying on heroku.
 
 if HEROKU:
     from os import environ
-
+    session_name = os.environ.get("session_name", None)
     bot_token = environ["bot_token"]
     ARQ_API_KEY = environ["ARQ_API_KEY"]
     LANGUAGE = environ["LANGUAGE"]
